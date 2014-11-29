aa8686a0ca14aae5769b5ead42c0cded130f221a
SamLangTen <samlangten@outlook.com>
2014-11-29 20:17:32 +0800

    Add:key binding dialog

---
+++ RtCWGH/ViewModels/KeyBindingEditorViewModel.vb
@@ -0,0 +1,31 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports GalaSoft.MvvmLight.Command
+Imports GalaSoft.MvvmLight.Messaging
+Public Class KeyBindingEditorViewModel
+    Implements INotifyPropertyChanged
+
+    Public Event PropertyChanged(sender As Object, e As PropertyChangedEventArgs) Implements INotifyPropertyChanged.PropertyChanged
+
+    Private _CommandLists As New List(Of String)
+    Public Property CommandLists As List(Of String)
+        Set(value As List(Of String))
+            _CommandLists = value
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs("CommandLists"))
+        End Set
+        Get
+            Return _CommandLists
+        End Get
+    End Property
+    Public ReadOnly Property KeyLists As List(Of String)
+        Get
+            Return [Enum].GetNames(GetType(Key)).ToList()
+        End Get
+    End Property
+
+
+    Public Property SelectedCommand As String
+
+    Public Property SelectedKey As String
+
+End Class
--- RtCWGH/Views/KeyBindingEditor.xaml.vb
+++ RtCWGH/Views/KeyBindingEditor.xaml.vb
@@ -1,3 +1,10 @@
 ﻿Public Class KeyBindingEditor
+    Sub New()
 
+        ' 此调用是设计器所必需的。
+        InitializeComponent()
+
+        ' 在 InitializeComponent() 调用之后添加任何初始化。
+        Me.DataContext = New KeyBindingEditorViewModel
+    End Sub
 End Class
