c548e86f5ebecaece6ced5159bff53b4d6039460
SamLangTen <samlangten@outlook.com>
2016-11-03 12:26:31 +0800

    Update: renamed mappackage

--- RTCWGH/Classes/PK3Manager/MapPackage.vb
+++ RTCWGH/Classes/PK3Manager/MapPackage.vb
@@ -5,7 +5,7 @@ Namespace Game
     ''' <summary>
     ''' Represent a series maps in a pk3 with independent save and setting folder
     ''' </summary>
-    Public Class MapPackage
+    Public Class MissionPackage
 
         Public Property MapPK3s As New List(Of MapPK3File)
         Public Property CustomComponents As New List(Of String)
